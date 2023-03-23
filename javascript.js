fd236249dc953e300168826c7fac1c9a492f1c39
PramodK2808 <pramodkushwaha1328@gmail.com>
2023-03-23 15:53:18 +0530

    code

--- javascript.js
+++ javascript.js
@@ -5,8 +5,8 @@ let navbar = document.querySelector(".navbar");
 
 
 window.addEventListener("scroll", () => {
-    header.classList.toggle("shadow",window.scrollY > 0)
-
+    header.classList.toggle("shadow", window.scrollY > 0)
+    
 })
 
 const darkMode = document.querySelector("#darkBtn");
